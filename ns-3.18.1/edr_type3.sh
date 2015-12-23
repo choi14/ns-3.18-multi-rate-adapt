bd0ada7bd7a0fc57185e3199220d5776540fed7e
choi14 <jychoi@mwnl.snu.ac.kr>
2015-12-23 18:57:09 +0900

    151223 jychoi inter

---
+++ ns-3.18.1/edr_type3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+clear
+
+iter_start=$1
+iter_end=$2
+
+for seed in $(seq $iter_start $iter_end)
+do
+	for feedbackType in 4
+	do
+		for blockSize in 1 5 10
+		do
+			for bound in 10 20
+			do
+				for EDRtype in 3  
+				do
+					for linearTime in 2000
+					do
+						for rxNodeNum in 10 
+						do
+							for period in 1000 
+							do
+								for doppler in 0.1 
+								do
+									for eta in 1 2 3 4 5
+									do
+										for delta in 0.1
+										do
+											for rho in 0.1 0.9
+											do
+												./waf --run "scratch/test_r2 --Seed=$seed --FeedbackType=$feedbackType --BlockSize=$blockSize --Bound=$bound --EDRtype=$EDRtype --LinearTime=$linearTime --RxNodeNum=$rxNodeNum --FeedbackPeriod=$period --Doppler=$doppler --Eta=$eta --Delta=$delta --Rho=$rho"
+											done
+										done
+									done
+								done
+							done
+						done
+					done
+				done
+			done
+		done
+	done
+done
